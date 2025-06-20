79b84d8c4b8121796dd6c773ac8b19a28f9199c6
Mateusz Konat <polakwmc@gazeta.pl>
2025-06-20 17:17:49 +0200

    add training exercises

---
+++ lesson_3/loop_game.py
@@ -0,0 +1,13 @@
+
+if __name__ == "__main__":
+    while True:
+        user_letter = input("Type letter:")
+        
+        if user_letter == "l":
+            print("Bravo! You typed 'l'!!!!")
+            break
+        else:
+            print("Wrong letter. SORRY.")
+        
+    print("Game finished")
+    
---
+++ lesson_3/user_input.py
@@ -0,0 +1,6 @@
+
+if __name__ == "__main__":
+    name = input("Type your name: ")
+    print(type(name))
+    
+    print("Hello,", name)
