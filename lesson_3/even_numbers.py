679be5fb242401a9daee0951c8fc29fdb53d3705
Mateusz Konat <polakwmc@gazeta.pl>
2025-09-28 09:18:01 +0200

    Nowa lekcja

--- lesson_3/even_numbers.py
+++
@@ -1,9 +0,0 @@
-numbers = [1, 13, 2, 10, 5]
-doubled_even = []
-
-for element in numbers:
-    if element % 2 == 0:
-        doubled_even.append(element * 2)
-        
-
-print(doubled_even)
--- lesson_3/user_input.py
+++
@@ -1,6 +0,0 @@
-
-if __name__ == "__main__":
-    name = input("Type your name: ")
-    print(type(name))
-    
-    print("Hello,", name)
