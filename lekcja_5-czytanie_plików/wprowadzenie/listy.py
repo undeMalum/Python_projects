c2a1479d19984c8047957b9931394ae5395e53a9
Mateusz Konat <polakwmc@gazeta.pl>
2025-10-22 17:36:09 +0200

    Wyjasnij listy

--- lekcja_5-czytanie_plików/wprowadzenie/listy.py
+++ lekcja_5-czytanie_plików/wprowadzenie/listy.py
@@ -6,10 +6,11 @@ print(a[-3])
 
 print(a[1:3]) # <1,3),  range(1,3) -> 1, 2
 
+print("----")
 print(a[1:-1])
+print(a[1:])
 
 print(a[3:5])
-
 
 print(a[:3])
 
@@ -25,5 +26,22 @@ b = ["i", "j", "k", "l", "m", "n", "o", "p"]
 
 print(a + b)
 # myinvers(["b", "c", "d"]) + ["a"]
-# myinvers(["c", "d"]) + ["b"]
+# myinvers(---------------)  + [lista[0]]
+
+# mvinvers(["c", "d"]) + [lista[0]]       + ["a"]
+# myinvers(["c", "d"]) + ["b"] + ["a"]
+
+
+# myinvers(lista[1:]) + [lista[0]]
+
+
+"""
+1. list = ["a", "b", "c", "d"]
+a. myinvers(lista[1:]) + [lista[0]]
+   myinvers(["b", "c", "d"]) + ["a"]
+   +----------------------------------> 2. lista = ["b", "c", "d"]
+                                        a. myinvers(lista[1:]) + [lista[0]]
+                                           myinvers(["c", "d"]) + ["b"]
+                                           +-----------------------------------> 3. lista = ["c", "d"]
+"""
 
