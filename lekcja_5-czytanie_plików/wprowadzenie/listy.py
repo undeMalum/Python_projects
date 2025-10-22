30c3a349d05d5748614f91f0e7a12b2209d48544
Mateusz Konat <polakwmc@gazeta.pl>
2025-10-22 17:17:03 +0200

    Nowe zadanie + wprowadzenie list

---
+++ lekcja_5-czytanie_plików/wprowadzenie/listy.py
@@ -0,0 +1,29 @@
+a = ["a", "b", "c", "d", "e", "f", "g", "h"]
+
+print(a[3])
+
+print(a[-3])
+
+print(a[1:3]) # <1,3),  range(1,3) -> 1, 2
+
+print(a[1:-1])
+
+print(a[3:5])
+
+
+print(a[:3])
+
+print(a[1:6:2])
+
+print(a[:7:3])
+
+print(a[-1:3:-1])
+
+print(a[::-1])
+
+b = ["i", "j", "k", "l", "m", "n", "o", "p"]
+
+print(a + b)
+# myinvers(["b", "c", "d"]) + ["a"]
+# myinvers(["c", "d"]) + ["b"]
+
